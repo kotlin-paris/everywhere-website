8262fcf38c9323094b29d0e552d8ad391734ecf8
Salomon BRYS <salomon@kodein.net>
2019-07-29 08:31:17 -0400

    New silver sponsor: Kaibee!

--- src/main/kotlin/paris/kotlin/everywhere/mainpage/Sponsors.kt
+++ src/main/kotlin/paris/kotlin/everywhere/mainpage/Sponsors.kt
@@ -2,13 +2,20 @@ package paris.kotlin.everywhere.mainpage
 
 
 import kotlinx.css.*
-import kotlinx.css.properties.boxShadow
 import kotlinx.css.properties.boxShadowInset
-import kotlinx.html.P
 import paris.kotlin.everywhere.MainPage
-import react.*
-import react.dom.*
-import styled.*
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+import react.dom.a
+import react.dom.div
+import react.dom.h1
+import react.dom.h2
+import styled.css
+import styled.styledDiv
+import styled.styledH1
+import styled.styledImg
 
 class Sponsors : RComponent<Sponsors.Props, RState>() {
 
@@ -20,27 +27,23 @@ class Sponsors : RComponent<Sponsors.Props, RState>() {
             ref = props.scrollTo
             css {
                 margin(20.px, 0.px)
-                padding(2.em, 0.px)
+                padding(1.em, 0.px)
                 backgroundColor = Color.silver
                 boxShadowInset(Color.black, blurRadius = 3.px)
-            }
 
-            styledH1 {
-                css {
+                "h1" {
                     textAlign = TextAlign.center
                     color = Color.dimGray
+                    paddingTop = 1.em
                 }
 
-                +"Sponsors"
-            }
-
-            styledDiv {
-                css {
+                "div" {
                     display = Display.flex
                     flexDirection = FlexDirection.row
                     justifyContent = JustifyContent.center
                     flexWrap = FlexWrap.wrap
                     alignItems = Align.center
+                    padding(0.px, 1.em)
 
                     "a" {
                         margin(1.em)
@@ -49,19 +52,36 @@ class Sponsors : RComponent<Sponsors.Props, RState>() {
                         }
                     }
                 }
+            }
 
+            h1 {
+                +"Organisateurs"
+            }
+
+            div {
                 a(href = "https://kodein.net") {
                     styledImg(alt = "Kodein Koders", src = "kodein-logo.svg") {
                         css { maxHeight = 70.px }
                     }
                 }
-                a(href = "https://epitech.eu/") {
+                a(href = "https://epitech.eu") {
                     styledImg(alt = "Epitech", src = "epitech-logo.png") {
                         css { maxHeight = 90.px }
                     }
                 }
             }
 
+            h1 {
+                +"Sponsors Silver"
+            }
+
+            div {
+                a(href = "https://kaibee.fr") {
+                    styledImg(alt = "Keibee", src = "kaibee-logo.png") {
+                        css { maxHeight = 100.px }
+                    }
+                }
+            }
 
         }
 
